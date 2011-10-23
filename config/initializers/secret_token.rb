4421c83c4668ee92a936a23306cb0c737900748b
Tad Thorley <phaedryx@gmail.com>
2011-10-23 12:07:14 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doogie::Application.config.secret_token = 'b814faab02c9eea6dab366b1cc8d328ce1d2cc9d084916fe047c738466548eff400390de024a4cc6034caec0d93fcbbbf5f62c4d506cb526f1677b1791169478'
